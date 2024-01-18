779b051597203a70c96d5108648a1722343d6823
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2024-01-18 23:11:10 +0000

    Add url paths for order-confirm and confirm-order-payment

--- eGrub/egrub/egrub/urls.py
+++ eGrub/egrub/egrub/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from customers.views import IndexPage, AboutPage, Order
+from customers.views import IndexPage, AboutPage, Order, ConfirmOrder, ConfirmOrderPayment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPage.as_view(), name='index'),
     path('about/', AboutPage.as_view(), name='about'),
     path('order/', Order.as_view(), name='order'),
+    path('order-confirm/<int:pk>', ConfirmOrder.as_view(), name='order-confirm'),
+    path('confirm-order-payment/', ConfirmOrderPayment.as_view(), name='confirm-order-payment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
