c9be49b23c851a6f7260b013c487c2c6a263f49e
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2024-01-19 21:56:04 +0000

    Add path for allauth accounts and import include

--- eGrub/egrub/egrub/urls.py
+++ eGrub/egrub/egrub/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from customers.views import IndexPage, AboutPage, Order, ConfirmOrder, ConfirmOrderPayment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', IndexPage.as_view(), name='index'),
     path('about/', AboutPage.as_view(), name='about'),
     path('order/', Order.as_view(), name='order'),
