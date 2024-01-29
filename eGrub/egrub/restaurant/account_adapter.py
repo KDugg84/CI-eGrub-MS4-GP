c51bec812ed1f432047e87e78ba2f88d4db12f66
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2024-01-29 14:05:13 +0000

    Add PEP8 spacing

--- eGrub/egrub/restaurant/account_adapter.py
+++ eGrub/egrub/restaurant/account_adapter.py
@@ -1,5 +1,6 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
+
 # Disable alluth 'Signup', see ACCOUNT_ADAPTER in settings.py
 class NoNewUsers(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
