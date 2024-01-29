343db44d115edda0dd39e8a31158c620682ee6ae
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2024-01-29 13:55:36 +0000

    Add PEP8 spacing

--- eGrub/egrub/customers/admin.py
+++ eGrub/egrub/customers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import MenuItems, Category, OrderModel
 
+
 admin.site.register(MenuItems)
 admin.site.register(Category)
 admin.site.register(OrderModel)
